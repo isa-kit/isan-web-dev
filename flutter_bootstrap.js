693e90df6ee953ee32a4069764cdc8028ff62fc3
tchahnia <186840118+tchahnia@users.noreply.github.com>
2026-06-30 22:01:55 -0400

    deploy isan web [dev] 2026-07-01T02:01:55Z

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3376467646" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3053571287" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
